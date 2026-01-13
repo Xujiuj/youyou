4cf7f60621a6364e4259ae08fcffc1ccf2b3742e
XuJiu <topjava@vip.qq.com>
2026-01-13 10:58:48 +0800

    v1.0.1 修改了不同分辨率的显示问题

--- .next/types/app/gallery/page.ts
+++ .next/types/app/gallery/page.ts
@@ -1,4 +1,4 @@
-// File: G:\software\youyou\app\gallery\page.tsx
+// File: D:\youyou\app\gallery\page.tsx
 import * as entry from '../../../../app/gallery/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
